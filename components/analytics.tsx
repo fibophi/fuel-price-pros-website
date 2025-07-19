5bc67dfd558daf57eabe837bd911924586624b52
fibophi <phibologic@gmail.com>
2025-07-18 20:09:42 -0400

    Create analytics.tsx

---
+++ components/analytics.tsx
@@ -0,0 +1,24 @@
+import Script from "next/script"
+import { GA_TRACKING_ID } from "@/lib/gtag"
+
+export function Analytics() {
+  return (
+    <>
+      <Script strategy="afterInteractive" src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`} />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+    </>
+  )
+}
